9cd1979edffd0d2f628163bddc79d51131aefcd0
George Abordioaie <abordioaie.george@yahoo.com>
2016-01-02 18:10:01 +0200

    Added Unit Test Project

---
+++ ServiceProvider/ServiceProviderUnitTests/CustomerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceProviderUnitTests
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
